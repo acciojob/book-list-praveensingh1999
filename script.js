e339ae25e75c9e42c9d16c11bdc82f5aad42ad6f
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-08-17 14:59:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-//your JS code here. If required.
+
+	
+
+let data=[];
+const handleSubmit = (evt) => {
+  evt.preventDefault()
+  const formData = new FormData(evt.currentTarget)
+  const formObject = Object.fromEntries(formData.entries())
+
+  
+  data.push(formObject);
+  console.log(data);
+
+  // table data show in table
+  document.querySelector("tbody").innerHTML="";
+  for(let i=0;i<data.length;i++){
+
+    const table = document.querySelector("tbody");
+    
+    const tr = document.createElement("tr");
+    const title= document.createElement("td");
+    title.textContent=data[i].title;
+    const author = document.createElement("td");
+    author.textContent=data[i].author;
+    const isbn = document.createElement("td");
+    isbn.textContent=data[i].isbn;
+    const btn = document.createElement("button");
+    btn.textContent="X";
+
+    tr.appendChild(title);
+    tr.appendChild(author);
+    tr.appendChild(isbn);
+    tr.appendChild(btn);
+    table.appendChild(tr);
+     btn.addEventListener("click", () => {
+      tr.remove();
+      data.splice(i, 1);
+      console.log("Updated data:", data);
+      //renderTable(); // re-render table with updated data
+    });
+
+  }
+}
